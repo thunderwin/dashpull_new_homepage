e95746cb70a0dac2463ac88713e9479960ceb580
Lbraim <lbraimbranco@gmail.com>
2025-03-14 22:16:36 +0800

    Initial commit

---
+++ app/pages/pricing.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('pricing', () => queryCollection('pricing').first())
+
+useSeoMeta({
+  title: page.value?.title,
+  ogTitle: page.value?.title,
+  description: page.value?.description,
+  ogDescription: page.value?.description
+})
+
+defineOgImageComponent('Saas')
+
+const isYearly = ref('0')
+
+const items = ref([
+  {
+    label: 'Monthly',
+    value: '0'
+  },
+  {
+    label: 'Yearly',
+    value: '1'
+  }
+])
+</script>
+
+<template>
+  <div v-if="page">
+    <UPageHero v-bind="page.hero">
+      <template #links>
+        <UTabs
+          v-model="isYearly"
+          :items="items"
+          color="neutral"
+          class="w-72"
+          :ui="{ list: 'rounded-full', indicator: 'rounded-full' }"
+        />
+      </template>
+    </UPageHero>
+
+    <UContainer>
+      <UPricingPlans scale>
+        <UPricingPlan
+          v-for="(plan, index) in page.plans"
+          :key="index"
+          v-bind="plan"
+          :price="isYearly === '1' ? plan.price.year : plan.price.month"
+          :billing-cycle="isYearly === '1' ? '/year' : '/month'"
+        />
+      </UPricingPlans>
+    </UContainer>
+
+    <UPageSection>
+      <UPageLogos>
+        <UIcon
+          v-for="icon in page.logos.icons"
+          :key="icon"
+          :name="icon"
+          class="w-12 h-12 flex-shrink-0 text-(--ui-text-muted)"
+        />
+      </UPageLogos>
+    </UPageSection>
+
+    <UPageSection
+      :title="page.faq.title"
+      :description="page.faq.description"
+    >
+      <UPageAccordion
+        :items="page.faq.items"
+        multiple
+        class="max-w-4xl mx-auto"
+      />
+    </UPageSection>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,46 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/ui-pro',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    'nuxt-og-image'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/',
+        '/docs'
+      ],
+      crawlLinks: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
